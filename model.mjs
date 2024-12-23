14a293926fb6e760e5acb218d7e215d1eb192840
Sereda Vladyslav <vlad.sereda03@gmail.com>
2024-12-23 18:06:05 +0200

    3rd task

--- model.mjs
+++ model.mjs
@@ -1,6 +1,10 @@
-function calculateNormals(vertices, indices) {
-    const vertexCount = vertices.length / 3;
+function normalizeUV(value, min, max) {
+    return (value - min) / (max - min);
+}
+
+function calculateNormalsAndTangents(vertices, indices, uvs) {
     const normals = new Float32Array(vertices.length).fill(0);
+    const tangents = new Float32Array(vertices.length).fill(0);
 
     for (let i = 0; i < indices.length; i += 3) {
         const i1 = indices[i] * 3;
@@ -13,6 +17,21 @@ function calculateNormals(vertices, indices) {
 
         const edge1 = [v2[0] - v1[0], v2[1] - v1[1], v2[2] - v1[2]];
         const edge2 = [v3[0] - v1[0], v3[1] - v1[1], v3[2] - v1[2]];
+
+        const uv1 = [uvs[indices[i] * 2], uvs[indices[i] * 2 + 1]];
+        const uv2 = [uvs[indices[i + 1] * 2], uvs[indices[i + 1] * 2 + 1]];
+        const uv3 = [uvs[indices[i + 2] * 2], uvs[indices[i + 2] * 2 + 1]];
+
+        const deltaUV1 = [uv2[0] - uv1[0], uv2[1] - uv1[1]];
+        const deltaUV2 = [uv3[0] - uv1[0], uv3[1] - uv1[1]];
+
+        const f = 1.0 / (deltaUV1[0] * deltaUV2[1] - deltaUV1[1] * deltaUV2[0]);
+
+        const tangent = [
+            f * (deltaUV2[1] * edge1[0] - deltaUV1[1] * edge2[0]),
+            f * (deltaUV2[1] * edge1[1] - deltaUV1[1] * edge2[1]),
+            f * (deltaUV2[1] * edge1[2] - deltaUV1[1] * edge2[2])
+        ];
 
         const normal = m4.normalize(m4.cross(edge1, edge2, [0, 1, 0]), []);
 
@@ -27,6 +46,18 @@ function calculateNormals(vertices, indices) {
         normals[i3] += normal[0];
         normals[i3 + 1] += normal[1];
         normals[i3 + 2] += normal[2];
+
+        tangents[i1] += tangent[0];
+        tangents[i1 + 1] += tangent[1];
+        tangents[i1 + 2] += tangent[2];
+
+        tangents[i2] += tangent[0];
+        tangents[i2 + 1] += tangent[1];
+        tangents[i2 + 2] += tangent[2];
+
+        tangents[i3] += tangent[0];
+        tangents[i3 + 1] += tangent[1];
+        tangents[i3 + 2] += tangent[2];
     }
 
     for (let i = 0; i < normals.length; i += 3) {
@@ -34,19 +65,41 @@ function calculateNormals(vertices, indices) {
         const ny = normals[i + 1];
         const nz = normals[i + 2];
 
-        const length = Math.sqrt(nx * nx + ny * ny + nz * nz);
-        if (length > 0) {
-            normals[i] = nx / length;
-            normals[i + 1] = ny / length;
-            normals[i + 2] = nz / length;
+        const tx = tangents[i];
+        const ty = tangents[i + 1];
+        const tz = tangents[i + 2];
+
+        const normalLength = Math.sqrt(nx * nx + ny * ny + nz * nz);
+        const tangentLength = Math.sqrt(tx * tx + ty * ty + tz * tz);
+
+        if (normalLength > 0) {
+            normals[i] = nx / normalLength;
+            normals[i + 1] = ny / normalLength;
+            normals[i + 2] = nz / normalLength;
+        }
+
+        if (tangentLength > 0) {
+            tangents[i] = tx / tangentLength;
+            tangents[i + 1] = ty / tangentLength;
+            tangents[i + 2] = tz / tangentLength;
         }
     }
 
-    return normals;
+    return { normals, tangents };
 }
+
+
+function createPoint(a, b, n, u, v) {
+    let x = (a + b * Math.sin(n * u)) * Math.cos(u) - v * Math.sin(u);
+    let y = (a + b * Math.sin(n * u)) * Math.sin(u) + v * Math.cos(u);
+    let z = b * Math.cos(n * u);
+
+    return [x, y, z]
+}  
 
 function generateSurface(a, b, n, uSteps, vSteps, vMin, vMax) {
     const vertices = [];
+    let uvs = [];
     const indices = [];
 
     const uMin = 0.0, uMax = 2.0 * Math.PI;
@@ -54,17 +107,29 @@ function generateSurface(a, b, n, uSteps, vSteps, vMin, vMax) {
     const du = (uMax - uMin) / uSteps;
     const dv = (vMax - vMin) / vSteps;
 
+    let accomulatedU = 0.0;
+
     for (let i = 0; i <= uSteps; i++) {
         const u = uMin + i * du;
+
+        {
+            const current = createPoint(a, b, n, u, vMin);
+            const prev = createPoint(a, b, n, u - du, 0);
+            
+            accomulatedU += m4.length(m4.subtractVectors(current, prev)) * 0.025;
+        }
         
+        let accomulatedV = 0.0;
+
         for (let j = 0; j <= vSteps; j++) {
             const v = vMin + j * dv;
 
-            let x = (a + b * Math.sin(n * u)) * Math.cos(u) - v * Math.sin(u);
-            let y = (a + b * Math.sin(n * u)) * Math.sin(u) + v * Math.cos(u);
-            let z = b * Math.cos(n * u);
+            const current = createPoint(a, b, n, u, v);
+            vertices.push(...current);
 
-            vertices.push(x, y, z);
+            const prev = createPoint(a, b, n, u, v - dv);
+            accomulatedV += m4.length(m4.subtractVectors(current, prev)) * 0.25;
+            uvs.push(accomulatedU, accomulatedV);
         }
     }
 
@@ -79,22 +144,35 @@ function generateSurface(a, b, n, uSteps, vSteps, vMin, vMax) {
             indices.push(topLeft, bottomRight, topRight);
         }
     }
-
-    return { vertices, indices };
+    
+    return { vertices, uvs, indices };
 }
 
 export default function Model(gl, shProgram) {
     this.iVertexBuffer = gl.createBuffer();
     this.iNormalBuffer = gl.createBuffer();
+    this.iTangentBuffer = gl.createBuffer();
+    this.iUVBuffer = gl.createBuffer();
     this.iIndexBuffer = gl.createBuffer();
+
+    this.idTextureDiffuse = LoadTexture(gl, "./textures/diffuse.jpg");
+    this.idTextureNormal = LoadTexture(gl, "./textures/normal.jpg");
+    this.idTextureSpecular = LoadTexture(gl, "./textures/specular.jpg");
+
     this.count = 0;
 
-    this.BufferData = function(vertices, normals, indices) {
+    this.BufferData = function(vertices, normals, tangents, uvs, indices) {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iTangentBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, tangents, gl.STATIC_DRAW);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iUVBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvs), gl.STATIC_DRAW);
 
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
@@ -111,6 +189,25 @@ export default function Model(gl, shProgram) {
         gl.vertexAttribPointer(shProgram.iAttribNormal, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribNormal);
 
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iTangentBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribTangent, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribTangent);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iUVBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribUV, 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribUV);
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, this.idTextureDiffuse);
+
+        gl.activeTexture(gl.TEXTURE1);
+        gl.bindTexture(gl.TEXTURE_2D, this.idTextureNormal);
+
+        gl.activeTexture(gl.TEXTURE2);
+        gl.bindTexture(gl.TEXTURE_2D, this.idTextureSpecular);
+
         gl.drawElements(gl.TRIANGLES, this.count, gl.UNSIGNED_SHORT, 0);
     }
 
@@ -119,8 +216,8 @@ export default function Model(gl, shProgram) {
             return parseFloat(document.getElementById(name).value);
         }
 
-        const { vertices, indices } = generateSurface(get('A'), get('B'), get('N'), get('USeg'), get('VSeg'), get('VMin'), get('VMax'));
-        const normals = calculateNormals(vertices, indices);
-        this.BufferData(vertices, normals, indices);
+        const { vertices, uvs, indices } = generateSurface(get('A'), get('B'), get('N'), get('USeg'), get('VSeg'), get('VMin'), get('VMax'));
+        const {normals, tangents} = calculateNormalsAndTangents(vertices, indices, uvs);
+        this.BufferData(vertices, normals, tangents, uvs, indices);
     }
 }
